e5786dcfa9def5a6cdc1388a0fe386f6d3dc4e52
AlannZhang <64608645+AlannZhang@users.noreply.github.com>
2020-12-05 18:20:24 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', () => { 
-    //console.log("Hello World"); 
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d"); 
+
+    const blackButton = document.querySelector(".black");
+    const redButton = document.querySelector(".red");
+    const blueButton = document.querySelector(".blue");
+    const yellowButton = document.querySelector(".yellow");
+    const greenButton = document.querySelector(".green");
+    const clearButton = document.querySelector(".clear");
 
     //Resizing
     canvas.height = window.innerHeight; 
@@ -9,14 +15,15 @@ window.addEventListener('load', () => {
 
     //variables and functions
     let drawing = false; 
+    let strokeColour = "black"; 
 
-    //When  mouse is clicked, drawing becomes true allowing user to draw
+    //When mouse is clicked, drawing becomes true allowing user to draw
     function startPosition(e){
         drawing = true; 
         draw(e);
     }
 
-    //When  mouse is not clicked, drawing becomes false stopping the drawing
+    //When mouse is not clicked, drawing becomes false stopping the drawing
     function finishedPosition() { 
         drawing = false; 
         ctx.beginPath();
@@ -31,14 +38,46 @@ window.addEventListener('load', () => {
         ctx.lineCap = "round";
         ctx.lineTo(e.clientX, e.clientY); 
         ctx.stroke();
+        ctx.strokeStyle = strokeColour;
         ctx.beginPath(); 
         ctx.moveTo(e.clientX, e.clientY);
     }
 
+    function changeToBlack () { 
+        strokeColour = "black";
+    }
+
+    function changeToRed () { 
+        strokeColour = "red";
+    }
+
+    function changeToBlue () { 
+        strokeColour = "blue";
+    }
+
+    function changeToYellow () { 
+        strokeColour = "yellow";
+    }
+
+    function changeToGreen () { 
+        strokeColour = "green";
+    }
+
+    //clears canvas when the erase button is pressed
+    function eraseCanvas () { 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
     //event listeners
+    //Drawing event listeners
     canvas.addEventListener("mousedown", startPosition); 
     canvas.addEventListener("mouseup", finishedPosition); 
     canvas.addEventListener("mousemove", draw); 
+
+    blackButton.addEventListener("click", changeToBlack);
+    redButton.addEventListener("click", changeToRed);
+    blueButton.addEventListener("click", changeToBlue);
+    yellowButton.addEventListener("click", changeToYellow);
+    greenButton.addEventListener("click", changeToGreen);
+    clearButton.addEventListener("click", eraseCanvas);
 }); 
-
-
